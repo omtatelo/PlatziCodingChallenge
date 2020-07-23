9e2fe3d973a56d356b9153b510186cc7050a3929
Shelley Valdés <35204421+omtatelo@users.noreply.github.com>
2020-07-23 05:54:54 -0500

    UX

--- rpsls.py
+++ rpsls.py
@@ -17,9 +17,9 @@ import random
 
 print("\nRock, Paper, Scissors, Lizard, Spock.\n\nScissors cut paper,\npaper covers rock,\nrock crushes lizard,\nlizard poisons Spock,\nSpock smashes scissors,\nscissors decapitates lizard,\nlizard eats paper,\npaper disproves Spock,\nSpock vaporizes rock,\nand —as it always has— rock crushes scissors.\n\nRock = 1 | Paper = 2 | Scissors = 3 | Lizard = 4 | Spock = 5\n")
 
-win = "It seems that you've won this time."
-lose = "Oh... I win"
-even = "We're even"
+win = "It seems that you've won this time.\n"
+lose = "Oh... I win\n"
+even = "We're even\n"
 
 """ def print_win():
     print(win)
@@ -93,4 +93,32 @@ def play(user, sheldon):
     elif user == 5 and sheldon == 5:
         print(even)
 
+user1, user2, user3, user4, user5 = "Rock", "Paper", "Scissors", "Lizard", "Spock"
+sheldon1, sheldon2, sheldon3, sheldon4, sheldon5 = "Rock", "Paper", "Scissors", "Lizard", "Spock"
+
+def UX(user, sheldon):
+    print("\nYou choosed: ")
+    if user == 1:
+        print(user1)
+    elif user == 2:
+        print(user2)
+    elif user == 3:
+        print(user3)
+    elif user == 4:
+        print(user4)
+    elif user == 5:
+        print(user5)
+    print("and I ")
+    if sheldon == 1:
+        print(sheldon1)
+    elif sheldon == 2:
+        print(sheldon2)
+    elif sheldon == 3:
+        print(sheldon3)
+    elif sheldon == 4:
+        print(sheldon4)
+    elif sheldon == 5:
+        print(sheldon5)
+    print("\n")
+UX(user, sheldon)
 play(user, sheldon)
